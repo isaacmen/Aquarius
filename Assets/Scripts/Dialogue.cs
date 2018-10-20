48cd3f53b06e26a6348ee4065b601fc61ddf1734
Andrew Park <ajpark5@uci.edu>
2018-10-19 21:02:08 -0700

    Initial Dialog System
    Added the rudimentary system for the dialogue.

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+
+    public string name;
+
+    [TextArea(5,15)]
+    public string[] sentences;
+
+
+}
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,16 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
 
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+
+
 	// Use this for initialization
 	void Start () {
-		
+        sentences = new Queue<string>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("The first to speak will be " + dialogue.name);
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
+        animator.SetBool("IsOpen", false);
+    }
+
 }
