4fa0d5e81a94a8bdcacfea9081241a47c731a59f
Crystal <crystal.agerton2015@gmail.com>
2018-10-29 02:26:37 -0700

    UI elements

---
+++ Assets/Scripts/UI/SliderText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+    public Slider slider;
+
+    private void FixedUpdate()
+    {
+        Text txt = GetComponent<Text>();
+        txt.text = slider.value.ToString() + "/" + slider.maxValue.ToString();
+    }
+}
