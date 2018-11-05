5faef1eb390499eed85829c3cd00cec2942e1d55
Andrew Park <ajpark5@uci.edu>
2018-11-05 10:48:11 -0800

    Dialogue + Title Screen Tweaks
    I added the template for a fully functional branching dialogue system with choices and also added some more minor stuff for the title screen scene.

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -10,9 +10,19 @@ public class Dialog : MonoBehaviour {
     [TextArea(5, 15)]
     public string[] sentences;
     private int index;
+    private bool option1 = false;
+    private bool option2 = false;
+    private bool skip = false;
     public float typingSpeed;
+    public int conversationNumberChoice1;
+    public int conversationNumberChoice2;
+    private int conversationNumberChoice = 100;
+    //int textIndex = 0;
 
     public GameObject continueButton;
+    public GameObject choiceButton1;
+    public GameObject choiceButton2;
+    public int choiceNumber;
 
     void Start()
     {
@@ -22,8 +32,13 @@ public class Dialog : MonoBehaviour {
 
     private void Update()
     {
-        if(textDisplay.text == sentences[index]){
+        if(textDisplay.text == sentences[index] && index != choiceNumber){
             continueButton.SetActive(true);
+        }
+        if (option1 == true){
+            conversationNumberChoice = conversationNumberChoice1;
+        } else if (option2 == true) {
+            conversationNumberChoice = conversationNumberChoice2;
         }
     }
 
@@ -35,18 +50,41 @@ public class Dialog : MonoBehaviour {
         }
     }
 
+    public void choice1(){
+        option1 = true;
+    }
+    public void choice2(){
+        option2 = true;
+    }
+
     public void NextSentence(){
 
         continueButton.SetActive(false);
-
-        if (index < sentences.Length - 1){
-            index++;
+        choiceButton1.SetActive(false);
+        choiceButton2.SetActive(false);
+        //index < sentences.Length - 1
+        if (index < conversationNumberChoice){
+            if (option2 == true){
+                index += 2;
+            } else if (option1 == true && skip == true){
+                index += 2;
+            } else if (option1 == true){
+                skip = true;
+                index++;
+            } else {
+                index++;
+            }
             textDisplay.text = "";
             StartCoroutine(Type());
         } else {
             textDisplay.text = "";
             continueButton.SetActive(false);
         }
+        if (index == choiceNumber){
+            choiceButton1.SetActive(true);
+            choiceButton2.SetActive(true);
+        }
+        Debug.Log(index);
     }
 
 }
