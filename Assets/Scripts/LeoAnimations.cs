1399269aae97b6928f9f95306e852a60a5f99bd1
Andrew Park <ajpark5@uci.edu>
2018-11-09 15:18:13 -0800

    Added animation triggers
    Added the ability to call Leo's attacking animation

---
+++ Assets/Scripts/LeoAnimations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeoAnimations : MonoBehaviour {
+
+    Animator m_Animator;
+
+    void Start()
+    {
+        //Fetch the Animator from your GameObject
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Press the space key to play the "Jump" state
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Animator.Play("Attacking");
+        }
+    }
+}
