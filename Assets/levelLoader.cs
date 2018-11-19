184370b7750a4b108f065afe40f0b8dc69340923
Andrew Park <ajpark5@uci.edu>
2018-11-19 14:51:29 -0800

    Title Screen groundwork
    added the foundations for a loading screen and added basic title screen functionality

---
+++ Assets/levelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class levelLoader : MonoBehaviour
+{
+
+    public GameObject loadingScreen;
+    public Slider slider;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/mainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    public void PlayGameSing()
+    {
+        //disable.toggle = true;
+    }
+    public void Back()
+    {
+        //disable.toggle = false;
+    }
+    public void Options()
+    {
+        //DisableOptions.toggle = true;
+    }
+    /*public void PlayGameMult() {
+        SceneManager.LoadScene("NetworkLobby");
+    }
+    public void LevelSelectIsland(){
+        SceneManager.LoadScene("Mediterranean");
+    }
+    public void LevelSelectCave(){
+        SceneManager.LoadScene("Cave");
+    }
+    public void LevelSelectDesert(){
+        SceneManager.LoadScene("Desert");
+    }
+    public void LevelSelectDemo(){
+        SceneManager.LoadScene("Shooting_Scene");
+    }
+    public void LevelSelectSandbox(){
+        SceneManager.LoadScene("Model Junkyard");
+    }*/
+
+    public void playGame(){
+        SceneManager.LoadScene("Prototype");
+    }
+
+    public void QuitGame() {
+        Debug.Log ("Quit");
+        Application.Quit();
+    }
+}
