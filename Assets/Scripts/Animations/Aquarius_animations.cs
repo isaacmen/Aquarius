be67b71abdc5b1239538ed09efa6523a6ca7a01b
Andrew Park <ajpark5@uci.edu>
2018-11-26 14:58:00 -0800

    Aquarius animation + file organization
    Implemented animations for Aquarius and reorganized some project files.

---
+++ Assets/Scripts/Animations/Aquarius_animations.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aquarius_animations : MonoBehaviour
+{
+
+    Animator m_Animator;
+
+    void Start()
+    {
+        //Fetch the Animator from your GameObject
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Press the space key to play the "Attacking" state
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Animator.Play("Attacking");
+        } else if (Input.GetKey(KeyCode.DownArrow)){
+            m_Animator.Play("Hat");
+        }
+    }
+}
