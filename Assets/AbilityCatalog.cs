b51e56195c76e44545a0e93319b6decfe58e3418
Andrew Park <ajpark5@uci.edu>
2018-11-30 14:27:46 -0800

    AbilityCatalog
    Began work on a script that determines which abilities should be active for the game scene.

---
+++ Assets/AbilityCatalog.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityCatalog : MonoBehaviour {
+
+    public bool powerWordKill;
+    public bool lightning;
+    public bool crescendo;
+    public bool arpeggioPocoAPoco;
+    public bool layOnHands;
+    public bool shieldBash;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
