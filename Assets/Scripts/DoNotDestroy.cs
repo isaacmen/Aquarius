b5f375166ef19e4f6ca8771aed1efebac00611d1
Andrew Park <ajpark5@uci.edu>
2018-11-30 15:16:55 -0800

    Animation fixes
    fixed some animation bugs

---
+++ Assets/Scripts/DoNotDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script manages the ability catalog. The GameObject with the
+// "AbilityCatalog" tag is the preserved.
+
+public class DoNotDestroy : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("AbilityCatalog");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
