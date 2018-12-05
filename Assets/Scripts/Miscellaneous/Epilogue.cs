50da41abc21c5fbf7470e0adc550e35241d2d1c7
Isaac <isacmen@gmail.com>
2018-12-04 16:31:54 -0800

    SFX, epilogue done

---
+++ Assets/Scripts/Miscellaneous/Epilogue.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Epilogue : MonoBehaviour {
+
+	public Text endingText;
+	public Text groupText;
+	public string plotWrapUp;
+	public string leoEnding;
+	public string libraEnding;
+	public string scorpioEnding;
+	public string groupEnding;
+
+	public Image Portrait;
+	public Image Sprite;
+	public Image Frame;
+	public GameObject GroupSprite;
+	public Sprite LeoPortrait;
+	public Sprite LeoSprite;
+	public Sprite LibraPortrait;
+	public Sprite LibraSprite;
+	public Sprite ScorpioPortrait;
+	public Sprite ScorpioSprite;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (WriteSentence ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Q)) {
+			Debug.Log ("pressedt");
+			Application.Quit ();
+		}
+	}
+
+	IEnumerator WriteSentence ()
+	{
+		endingText.text = "";
+		foreach (char letter in plotWrapUp.ToCharArray())
+		{
+			endingText.text += letter;
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (1);
+
+		Portrait.sprite = LeoPortrait;
+		Sprite.sprite = LeoSprite;
+		endingText.text = "";
+		foreach (char letter in leoEnding.ToCharArray())
+		{
+			endingText.text += letter;
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (3);
+
+		Portrait.sprite = LibraPortrait;
+		Sprite.sprite = LibraSprite;
+		endingText.text = "";
+		foreach (char letter in libraEnding.ToCharArray())
+		{
+			endingText.text += letter;
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (3);
+
+		Portrait.sprite = ScorpioPortrait;
+		Sprite.sprite = ScorpioSprite;
+		endingText.text = "";
+		foreach (char letter in scorpioEnding.ToCharArray())
+		{
+			endingText.text += letter;
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (3);
+
+		GroupSprite.SetActive (true);
+		groupText.gameObject.SetActive (true);
+		Sprite.sprite = LeoSprite;
+
+		endingText.gameObject.SetActive (false);
+		Portrait.gameObject.SetActive (false);
+		Frame.gameObject.SetActive (false);
+
+		groupText.text = "";
+		foreach (char letter in groupEnding.ToCharArray())
+		{
+			groupText.text += letter;
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (3);
+
+		StopAllCoroutines();
+	}
+
+}
+
---
+++ Assets/Scripts/Miscellaneous/SFXPlayer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour {
+
+	public AudioClip jump;
+	public AudioClip AquaAttack;
+	public AudioClip AquaTidalCharge;
+	public AudioClip AquaTidalHit;
+	public AudioClip AquaSummon;
+	public AudioClip LeoAttack;
+	public AudioClip LeoUnload;
+	public AudioClip LeoLullaby;
+	public AudioClip LeoCrescendo;
+	public AudioClip ScorpioAttack;
+	public AudioClip ScorpioFireball;
+	public AudioClip ScorpioLightning;
+	public AudioClip ScorpioPWKCharge;
+	public AudioClip ScorpioPWKHit;
+	public AudioClip LibraAttack;
+	public AudioClip LibraGuard;
+	public AudioClip LibraLOH;
+	public AudioClip LibraShieldbash;
+
+
+
+	private AudioSource src;
+
+	// Use this for initialization
+	void Start () {
+		src = this.GetComponent<AudioSource> ();
+	}
+	
+	public void Jump (string skill) {
+		if (skill == "jump") {
+			src.clip = jump;
+		} else if (skill == "AquaAttack") {
+			src.clip = AquaAttack;
+		} else if (skill == "AquaTidalCharge") {
+			src.clip = AquaTidalCharge;
+		} else if (skill == "AquaTidalHit") {
+			src.clip = AquaTidalHit;
+		} else if (skill == "AquaSummon") {
+			src.clip = AquaSummon;
+		} else if (skill == "LeoAttack") {
+			src.clip = LeoAttack;
+		} else if (skill == "LeoUnload") {
+			src.clip = LeoUnload;
+		} else if (skill == "LeoLullaby") {
+			src.clip = LeoLullaby;
+		} else if (skill == "LeoCrescendo") {
+			src.clip = LeoCrescendo;
+		} else if (skill == "ScorpioAttack") {
+			src.clip = ScorpioAttack;
+		} else if (skill == "ScorpioFireball") {
+			src.clip = ScorpioFireball;
+		} else if (skill == "ScorpioLightning") {
+			src.clip = ScorpioLightning;
+		} else if (skill == "ScorpioPWKCharge") {
+			src.clip = ScorpioPWKCharge;
+		} else if (skill == "ScorpioPWKHit") {
+			src.clip = ScorpioPWKHit;
+		} else if (skill == "LibraAttack") {
+			src.clip = LibraAttack;
+		} else if (skill == "LibraGuard") {
+			src.clip = LibraGuard;
+		} else if (skill == "LibraLOH") {
+			src.clip = LibraLOH;
+		} else if (skill == "LibraShieldbash") {
+			src.clip = LibraShieldbash;
+		} 
+
+		src.Play ();
+	}
+
+}
